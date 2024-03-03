bd39331cf04f33368ec4d761261ac85ffa23e722
Vinicios <viniciosking309@gmail.com>
2024-03-03 11:36:48 -0300

    Mudança no carrossel.

--- js/filtro.js
+++ js/filtro.js
@@ -1,40 +1,28 @@
 $(document).ready(function() {
-    // Inicializar o Swiper
-    var swiper = new Swiper('.mySwiper', {
-        slidesPerView: 'auto', // Defina 'auto' para permitir que o Swiper ajuste dinamicamente o número de slides visíveis
-        spaceBetween: 70, // Adicione um espaçamento entre os slides
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-    
-
-    // Função para atualizar o carrossel com base no filtro
+    // Função para atualizar o carrossel com base no filtro de especialização
     function updateCarousel() {
         var especializacaoSelecionada = $('#filtro-especializacao').val();
         var numMedicos = 0;
 
-        // Verificar cada slide do carrossel e contar os médicos correspondentes à especialização selecionada
+        // Iterar sobre cada slide do carrossel
         $('.swiper-slide').each(function() {
             var especializacaoMedico = $(this).find('.profession').text();
             
+            // Verificar se a especialização do médico corresponde à opção selecionada ou se é 'todos'
             if (especializacaoSelecionada === 'todos' || especializacaoSelecionada === especializacaoMedico) {
                 numMedicos++;
-                $(this).show();
+                $(this).show(); // Mostrar o slide
             } else {
-                $(this).hide();
+                $(this).hide(); // Ocultar o slide
             }
         });
 
-        // Ajustar o número de slides visíveis com base na quantidade de médicos
-        console.log("Número de médicos antes do ajuste: " + numMedicos);
+        // Atualizar o número de slides visíveis com base na quantidade de médicos
         swiper.params.slidesPerView = Math.min(numMedicos, 3);
         swiper.update(); // Atualizar o swiper para refletir as mudanças
 
         // Retornar o carrossel à posição inicial
-        console.log("Retornando o carrossel à posição inicial");
-        swiper.slideTo(-1);
+        swiper.slideTo(0);
 
         // Configurar o carrossel para permitir ou não a navegação
         if (numMedicos <= 3) {
@@ -44,13 +32,10 @@ $(document).ready(function() {
             swiper.allowSlidePrev = true;
             swiper.allowSlideNext = true;
         }
-
-        console.log("Número de médicos atualizado: " + numMedicos);
     }
 
-    // Atualizar o carrossel quando o filtro for alterado
+    // Atualizar o carrossel quando o filtro de especialização for alterado
     $('#filtro-especializacao').change(function() {
-        console.log("Filtro alterado: " + $(this).val());
         updateCarousel();
     });
 
