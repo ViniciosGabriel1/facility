e1aeccfb1ee2e219ceada5f3d65c8134c8f74280
Vinicios <viniciosking309@gmail.com>
2024-03-05 20:50:28 -0300

    Mudança no layout do histórico de consultas.

--- paciente/historico_consulta.php
+++ paciente/historico_consulta.php
@@ -6,16 +6,10 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/historico_consultas.css">
     <title>Histórico de Consultas</title>
-    <style>
-        .consultas-pendentes {
-            color: white;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-    </style>
 </head>
 <body>
 <?php
+include "menu_paciente.php";
 session_start();
 include "../back/conexao.php";
 
@@ -30,7 +24,7 @@ if (!isset($_SESSION["id_usuario"])) {
 $id_paciente = $_SESSION["id_usuario"];
 
 // Consulta SQL para obter o histórico de consultas do paciente
-$sql = "SELECT id, data_consulta, id_medico, servico, observacoes, status FROM consultas WHERE id_paciente = ? ORDER BY FIELD(status, 'Agendada'), data_consulta DESC";
+$sql = "SELECT id, data_consulta, id_medico, servico, observacoes, status FROM consultas WHERE id_paciente = ? ORDER BY data_consulta DESC";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $id_paciente);
 $stmt->execute();
@@ -39,41 +33,66 @@ $stmt->bind_result($id_consulta, $data_consulta, $id_medico, $servico, $observac
 // Armazenar o resultado da consulta principal
 $stmt->store_result();
 
+// Variável para controlar se já foram exibidas as consultas pendentes
+$consultas_pendentes_exibidas = false;
+
 // Verificar se há consultas no histórico
 if ($stmt->num_rows > 0) {
     // Exibir consultas no histórico
-    $consultas_pendentes_impressas = false; // Flag para indicar se as consultas pendentes já foram impressas
     while ($stmt->fetch()) {
-        if ($status == 'Agendada' && !$consultas_pendentes_impressas) {
-            echo "<h3 class='consultas-pendentes'>Consultas Pendentes</h3>";
-            $consultas_pendentes_impressas = true; // Atualiza a flag para indicar que as consultas pendentes já foram impressas
-        }
         // Consulta para obter o nome do dentista
-        $sql_medico = "SELECT nome FROM medicos WHERE id = ?";
+        $sql_medico = "SELECT nome, telefone FROM medicos WHERE id = ?";
         $stmt_medico = $conn->prepare($sql_medico);
         $stmt_medico->bind_param("i", $id_medico);
         $stmt_medico->execute();
-        $stmt_medico->bind_result($nome_medico);
+        $stmt_medico->bind_result($nome_medico, $telefone_medico);
         
         // Verificar se encontrou o médico
         if ($stmt_medico->fetch()) {
-            ?>
-            <div class="consulta-card">
-                <p class="consulta-info"><strong>Data da Consulta:</strong> <?php echo $data_consulta; ?></p>
-                <p class="consulta-info"><strong>Médico:</strong> <?php echo $nome_medico; ?></p>
-                <p class="consulta-info"><strong>Serviço:</strong> <?php echo $servico; ?></p>
-                <p class="consulta-info"><strong>Observações:</strong> <?php echo $observacoes; ?></p>
-                <p class="consulta-info"><strong>Status:</strong> 
-                    <span class="<?php echo strtolower($status); ?>"><?php echo $status; ?></span>
-                </p>
-                <div class="consulta-botoes">
-                    <?php if ($status == 'Agendada') { ?>
-                        <button class="btn-cancelar">Cancelar</button>
-                        <button class="btn-reagendar">Reagendar</button>
-                    <?php } ?>
+            // Verificar se a consulta é pendente e se ainda não foram exibidas as consultas pendentes
+            if (($status == 'Agendada' || $status == 'Reagendada') && !$consultas_pendentes_exibidas) {
+                echo "<h3 class='consultas-pendentes'>Consultas Pendentes</h3>";
+                $consultas_pendentes_exibidas = true; // Marcamos que as consultas pendentes foram exibidas
+            }
+            // Exibir as consultas pendentes
+            if ($status == 'Agendada' || $status == 'Reagendada') {
+                ?>
+                <div class="consulta-card">
+                <p class="consulta-info"><strong>Data da Consulta:</strong> <?php echo date('d/m/Y H:i', strtotime($data_consulta)); ?></p>
+                    <p class="consulta-info"><strong>Médico:</strong> <?php echo $nome_medico; ?></p>
+                    <p class="consulta-info"><strong>Serviço:</strong> <?php echo $servico; ?></p>
+                    <p class="consulta-info"><strong>Observações:</strong> <?php echo $observacoes; ?></p>
+                    <p class="consulta-info"><strong>Status:</strong> 
+                        <span class="<?php echo strtolower($status); ?>"><?php echo $status; ?></span>
+                    </p>
+                    <div class="consulta-botoes">
+                        <?php if ($status == 'Agendada' || $status == 'Reagendada') { ?>
+                            <div class="whats"><p class="consulta-info"><a  target="_blank" style="color: green;" href="https://api.whatsapp.com/send?phone=<?php echo $telefone_medico; ?> ">Enviar mensagem pelo WhatsApp</a><i style="color: green;" class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i></p></div>
+
+                            <br><button class="btn-cancelar">Cancelar</button>
+                            <button class="btn-reagendar">Reagendar</button>
+                        <?php } ?>
+                    </div>
                 </div>
-            </div>
-            <?php
+                <?php
+            } else {
+                // Exibir as consultas concluídas
+                if ($consultas_pendentes_exibidas) {
+                    echo "<h3 class='consultas-concluidas'>Consultas Concluídas</h3>";
+                    $consultas_pendentes_exibidas = false; // Marcamos que as consultas concluídas foram exibidas
+                }
+                ?>
+                <div class="consulta-card">
+                    <p class="consulta-info"><strong>Data da Consulta:</strong> <?php echo $data_consulta; ?></p>
+                    <p class="consulta-info"><strong>Médico:</strong> <?php echo $nome_medico; ?></p>
+                    <p class="consulta-info"><strong>Serviço:</strong> <?php echo $servico; ?></p>
+                    <p class="consulta-info"><strong>Observações:</strong> <?php echo $observacoes; ?></p>
+                    <p class="consulta-info"><strong>Status:</strong> 
+                        <span class="<?php echo strtolower($status); ?>"><?php echo $status; ?></span>
+                    </p>
+                </div>
+                <?php
+            }
         } else {
             echo "<p>Nome do médico não encontrado.</p>";
         }
@@ -91,6 +110,5 @@ $stmt->free_result();
 $stmt->close();
 $conn->close();
 ?>
-    </div>
 </body>
 </html>
